0cc14e04142a206487823473449be90b7c50d479
Shighi <daisyshighi@gmail.com>
2024-09-17 00:39:44 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,10 @@
 # Import the add function from add_0.py
 from add_0 import add
 
-# Assign values to a and b
-a = 1
-b = 2
+if __name__ == "__main__":
+    # Assign values to a and b
+    a = 1
+    b = 2
 
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, add(a, b)))
-
+    # Print the result using string formatting
+    print("{} + {} = {}".format(a, b, add(a, b)))
