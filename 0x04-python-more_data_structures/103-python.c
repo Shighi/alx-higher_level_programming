39a1db479f09e22954bc2211a96e7c0bf180b481
Shighi <daisyshighi@gmail.com>
2024-09-19 00:43:26 -0700

    last

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -22,16 +22,13 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	if (PyBytes_AsStringAndSize(p, &string, &size) < 0)
-	{
-		printf("  [ERROR] Failed to retrieve string\n");
-		return;
-	}
+	size = PyBytes_Size(p);
+	string = PyBytes_AsString(p);
 
 	printf("  size: %zd\n", size);
 	printf("  trying string: %s\n", string);
 
-	printf("  first 10 bytes: ");
+	printf("  first %zd bytes: ", size < 10 ? size : 10);
 	for (i = 0; i < size && i < 10; i++)
 	{
 		printf("%02x", (unsigned char)string[i]);
@@ -47,21 +44,21 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, i;
+	Py_ssize_t size, alloc, i;
 	PyObject *item;
 	const char *type;
-	PyListObject *list = (PyListObject *)p;
 
-	size = PyObject_Length(p);
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", list->allocated);
+	printf("[*] Allocated = %zd\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
-		item = list->ob_item[i];
-		type = item->ob_type->tp_name;
+		item = PyList_GetItem(p, i);
+		type = Py_TYPE(item)->tp_name;
 		printf("Element %zd: %s\n", i, type);
 		if (PyBytes_Check(item))
 			print_python_bytes(item);
