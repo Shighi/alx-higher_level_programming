94036906564c08f388651a8ce53399474843dc86
Shighi <daisyshighi@gmail.com>
2024-10-14 08:16:10 -0700

    stl

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
     """
-    Returns True if the object is exactly an instance of the specified class; otherwise False.
+    Returns True if the object is exactly an instance of the specified class;
+    otherwise False.
 
     Args:
         obj: The object to check.
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if the object is an instance of, or inherited from, the specified class; otherwise False.
+    Returns True if the object is an instance of, or inherited from,
+    the specified class; otherwise False.
 
     Args:
         obj: The object to check.
         a_class: The class to compare against.
 
     Returns:
-        bool: True if obj is an instance of a_class or inherited from a_class, False otherwise.
+        bool: True if obj is an instance of a_class or inherited from a_class,
+        False otherwise.
     """
     return isinstance(obj, a_class)
