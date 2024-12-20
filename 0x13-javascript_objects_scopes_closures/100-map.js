a4c6c4b9489b49f67fee1485e2d12e9b081dc2ea
Shighi <daisyshighi@gmail.com>
2024-12-20 07:16:42 +0300

    advanced

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const { list } = require('./100-data');
-const newList = list.map((value, index) => value * index);
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((x, xI) => x * xI));
