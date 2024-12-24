73f7252b5270e7ed0f370de1bac4bc89e4c21d3c
Shighi <daisyshighi@gmail.com>
2024-12-23 22:27:42 -0800

    tzero

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
 from sys import argv
 
-# The code should not be executed when imported
-if __name__ == '__main__':
+if __name__ == "__main__":
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+        
+        # Create a cursor object to interact with the database
+        cur = db.cursor()
+        
+        # Execute the SQL query to fetch all states ordered by id
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        
+        # Fetch and print all the rows
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as err:
+        # Print any errors that occur during database operations
+        print(f"Error: {err}")
+    finally:
+        # Ensure resources are closed
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
 
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
-    cur.close()
-    db.close()
