c70d6d56642fd72222f93b8f1b639807b90a3ec2
Shighi <daisyshighi@gmail.com>
2025-01-16 20:03:27 -0800

    doc

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Finds a peak in a list of unsorted integers."""
 
+
 def find_peak(list_of_integers):
+    """Finds a peak element in a list of unsorted integers."""
     if not list_of_integers:
         return None
+
     left, right = 0, len(list_of_integers) - 1
     while left < right:
         mid = (left + right) // 2
