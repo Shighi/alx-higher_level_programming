788a9bee22f003915525802d682ca4142779e1a5
Shighi <daisyshighi@gmail.com>
2025-01-18 22:41:19 -0800

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-Script that fetches https://alx-intranet.hbtn.io/status using urllib package.
-
-This script demonstrates how to:
-    - Make a GET request using urllib
-    - Handle the response and read its content
-    - Display response body information including type and content
-"""
-import urllib.request
 
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
